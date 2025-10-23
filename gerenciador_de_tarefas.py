d2889e98c5395e5e4a638754b155190e9d99fab3
Elohaalves04 <elohadoloresferreiraalves07@gmail.com>
2025-10-23 08:52:54 -0300

    Add files via upload

---
+++ gerenciador_de_tarefas.py
@@ -0,0 +1,75 @@
+import os 
+
+tarefas = {
+    "tarefas" : ['Arrumar o quarto', 'Lavar a louça'],
+    "datas" : ['17/10', '17/10']
+}
+
+def mostrar_tarefa():
+    barra = f'|{60*'-'}|'
+    print(barra)
+    for i in range(len(tarefas['tarefas'])):
+        print(f'| {i+1} - Tarefa: {tarefas['tarefas'][i]} | Data: {tarefas['datas'][i]}')
+    input('| Aperte "ENTER" para continuar...')
+
+def adicionar_tarefa():
+    barra = f'|{60*'-'}|'
+    print(barra)
+    tarefa = input('Digite a tarefa: ')
+    data = input('Digite a data: ')
+    tarefas['tarefas'].append(tarefa)
+    tarefas['datas'].append(data)
+    input('| Aperte "ENTER" para continuar...')
+
+def remover_tarefa():
+    barra = f'|{60*'-'}|'
+    print(barra)
+
+    for i in range(len(tarefas['tarefas'])):
+        print(f'| {i+1} - Tarefa: {tarefas['tarefas'][i]} | Data: {tarefas['datas'][i]}')
+    id_tarefa = int(input('Digite a tarefa que você deseja remover: '))
+
+    if id_tarefa > 0:
+        tarefa = tarefas['tarefas'].pop(id_tarefa-1)
+        tarefas['datas'].pop(id_tarefa-1)
+    else: 
+        print('| ID inválido.')
+
+    print(f'| Tarefa "{tarefa}" removida com sucesso...')
+    input('| Aperte "ENTER" para continuar...')
+
+def menu(): 
+    while True: 
+        try: 
+            os.system('cls')
+            barra = f'|{60*'-'}|'
+            print(barra)
+            print('| GERENCIADOR DE TAREFAS')
+            print(barra)
+            print('| 1 - Mostrar tarefas')
+            print('| 2 - Adicionar tarefa ')
+            print('| 3 - Remover tarefa')
+            print('| 4 - Sair')
+            print(barra)
+            opc = int(input('| EScolha o número da opção: '))
+            if opc == 1: 
+                os.system('cls')
+                mostrar_tarefa()
+            elif opc == 2:
+                os.system('cls')
+                adicionar_tarefa()
+            elif opc == 3:
+                os.system('cls')
+                remover_tarefa()
+            elif opc == 4:
+                print('| Saindo do programa...')
+                break 
+            else: 
+                os.system('cls')
+                print('| Opção inválida')
+                input('| Aperte "ENTER" para continuar...')
+        except:
+            print('| ERRO! Uma opção inválida...')
+            input('| Aperte "ENTER" para continuar...')
+
+menu()
